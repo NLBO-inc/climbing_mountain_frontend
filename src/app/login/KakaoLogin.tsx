056f8c4e1044fd1b8e4ef80153251c5e54dad538
jeje01 <jje141@gmail.com>
2024-01-09 16:32:55 +0900

    feat: 카카오 로그인 추가

---
+++ src/app/login/KakaoLogin.tsx
@@ -0,0 +1,16 @@
+const KakaoLogin = () => {
+  // TODO: secret 처리
+  const REST_API_KEY = 'ed13807bf40fa18ae2ceb31e732c4b97'
+  const onClickKakaoLogin = async () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=http://localhost:3000/login&response_type=code`
+  }
+
+  return (
+    <button onClick={onClickKakaoLogin}>
+      로그인
+      <img src="/img/kakao_login_large_wide.png" alt="kakao login" />
+    </button>
+  )
+}
+
+export default KakaoLogin
---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import KakaoLogin from './KakaoLogin'
+
+const Login = () => {
+  return (
+    <div className="p-8">
+      <KakaoLogin />
+    </div>
+  )
+}
+
+export default Login
